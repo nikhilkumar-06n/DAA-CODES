4dcd5319aee2578c9ffc41a8651279d4e166a1d9
nikhilkumar-06n <nsangati@student.gitam.edu>
2026-03-04 08:38:45 +0530

    Fix formatting and clean up code in all_pair_shortest_path.c

--- all_pair_shortest_path.c
+++ all_pair_shortest_path.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
-
 #define INF 99999
-
 int main() {
     int n;
-
     printf("Enter number of vertices: ");
     scanf("%d", &n);
-
     int dist[n][n];
-
     printf("Enter adjacency matrix (use %d for INF):\n", INF);
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             scanf("%d", &dist[i][j]);
-
     for (int k = 0; k < n; k++)
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 if (dist[i][k] + dist[k][j] < dist[i][j])
                     dist[i][j] = dist[i][k] + dist[k][j];
-
     printf("Shortest distance matrix:\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -31,6 +24,5 @@ int main() {
         }
         printf("\n");
     }
-
     return 0;
 }
